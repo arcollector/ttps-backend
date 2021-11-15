8ba466eccca1ee2a9a516e62b609b80df78c6aaa
Martin Ruiz <arcollector@gmail.com>
2021-11-14 22:31:22 -0300

    fix: use process env port

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.use("/pdf", pdfroute2);
 
 app.use("/api/pdf", pdfroute);
 
-app.listen(8080, ()=>console.log("server is runnning"));
+app.listen(process.env.PORT || 8080, ()=>console.log("server is runnning"));
 
 module.export = app;
