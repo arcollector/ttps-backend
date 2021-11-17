27c7f6c95f27304d4bad8f96e783a0372370ca76
Martin Ruiz <arcollector@gmail.com>
2021-11-17 15:58:43 -0300

    fix: cors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const pdfroute = require('./routes/pdf');
 const pdfroute2 = require('./routes/pdf2');
+var cors = require('cors');
 
-
-const corsHeaders= require('./middleware/cors');
+//const corsHeaders= require('./middleware/cors');
 
 
 const app=express();
 
-app.options("*", corsHeaders);
-app.use(corsHeaders);
+app.use(cors());
+
+//app.options("*", corsHeaders);
+//app.use(corsHeaders);
 
 app.use(express.json());
 app.use("/pdf", pdfroute2);
